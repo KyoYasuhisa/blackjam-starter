cf232d10d10e5d8ce017df0765844748b4752a9f
Satoru Matsuzaka <44631219+satorumatsuzaka@users.noreply.github.com>
2019-04-07 16:49:40 +0900

    small changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,7 @@ export default {
   font-family 'Product Sans'
   font-style normal
   font-weight 400
+  font-display swap
   src local('Open Sans'), local('OpenSans'), url(https://fonts.gstatic.com/s/productsans/v5/HYvgU2fE2nRJvZ5JFAumwegdm0LZdjqr5-oayXSOefg.woff2) format('woff2')
 html 
   font-family 'Product Sans', 'Yu Gothic', 'YuGothic', sans-serif
