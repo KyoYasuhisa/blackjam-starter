708e2abe42437a61dd917c2a26519f31decc01f9
Satoru Matsuzaka <44631219+satorumatsuzaka@users.noreply.github.com>
2019-04-13 01:04:43 +0900

    swiper added

--- components/Swiper.vue
+++ components/Swiper.vue
@@ -10,34 +10,35 @@
            id="swiper-inner"
            v-touch:swipe.left="swipeLeft"
            v-touch:swipe.right="swipeRight">
-        <div v-for="post in posts"
-             :key="post.sys.id"
-             :post="post"
-             class="swiper-item">
-          <img :src="post.fields.image.fields.file.url" alt="thumbnail">   
-          <p class="category">{{ post.fields.category.fields.name }}</p>
-          <h1>{{ post.fields.title }}</h1>    
-          <div class="tags">
-            <p v-for="tag in post.fields.tags"
-              :key="tag.sys.id">
-              {{ tag.fields.name }}
-            </p>
-          </div>
+        <SwiperItem v-for="post in posts"
+                    :key="post.sys.id"
+                    :post="post"/>    
+      </div>    
+      <div v-if="posts.length > 1">
+        <div class="slide-btn left-btn">
+          <img src="/left-arrow.svg" alt="left-arrow">
+        </div>
+        <div class="slide-btn right-btn">
+          <img src="/right-arrow.svg" alt="left-arrow">
         </div>    
-      </div>         
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import Vue2TouchEvents from 'vue2-touch-events'
+import SwiperItem from '~/components/SwiperItem.vue'
 
 export default {
   props: ['posts','type'],
   data () {
     return {
-      currentSlide: 1
+      currentSlide: 1,
     }
+  },
+  components: {
+    SwiperItem
   },
   methods: {
     swipeLeft () {
@@ -68,9 +69,7 @@ export default {
 .swiper
   .swiper-wrapper
     width 100%
-    margin 0 auto
-    height 350px
-    background rgba(0,0,0,.08)
+    position relative
     .swiper-inner
       width 100%
       display flex
@@ -78,45 +77,18 @@ export default {
       justify-content flex-start
       transition .4s
       transition-timing-function ease-out
-      .swiper-item
-        position relative
-        width 100%
-        min-width 100%
-        height 300px
-        padding-top 50px
-        text-align center
-        overflow hidden
-        img
-          position absolute
-          width 300px
-          top 0
-          left 50%
-          margin-left -150px
-          opacity .3
-          z-index -1
-        .category 
-          display inline-block
-          height 35px
-          margin 0 auto
-          padding 0 20px
-          line-height 35px
-          background #555
-          color white
-          border-radius 20px
-        h1
-          font-size 1.5rem
-        .tags
-          display flex
-          flex-wrap nowrap
-          justify-content center
-          p
-            margin 5px
-            border 1px solid #555
-            height 25px
-            line-height 25px
-            font-size .8rem
-            padding 0 5px
-            border-radius 5px
+    .slide-btn
+      position absolute
+      top 150px
+      width 40px
+    .left-btn
+      left 10px
+    .right-btn
+      right 10px
+  @media (max-width: 768px)
+    .swiper-wrapper
+      .swiper-inner
+        height 200px
 </style>
 
 
---
+++ components/SwiperItem.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="swiper-item">
+    <img class="thumbnail"
+         :src="post.fields.image.fields.file.url" alt="thumbnail">  
+    <nuxt-link :to="{ name: 'posts-getby-query', 
+                      params: { 
+                        getby: 'category',
+                        query: post.fields.category.fields.slug } }"> 
+      <p class="category">{{ post.fields.category.fields.name }}</p>
+    </nuxt-link>  
+    <nuxt-link :to="{ name: 'posts-slug', 
+                      params: { slug: post.fields.slug } }">
+      <h1>{{ post.fields.title }}</h1>    
+    </nuxt-link>  
+    <div class="tags">
+      <p v-for="tag in post.fields.tags"
+         :key="tag.sys.id"
+         @click="$router.push({ name: 'posts-getby-query', 
+                                params: { 
+                                  getby: 'tag',
+                                  query: tag.sys.id } })">
+         {{ tag.fields.name }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['post']
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper-item
+  position relative
+  margin 0 auto
+  min-width 100%
+  height 300px
+  padding-top 50px
+  text-align center
+  overflow hidden
+  .thumbnail
+    position absolute
+    width 300px
+    top 0
+    left 50%
+    margin-left -150px
+    opacity .3
+    z-index -1
+  .category 
+    display inline-block
+    height 35px
+    margin 0 auto
+    padding 0 20px
+    line-height 35px
+    background #555
+    color white
+    border-radius 20px
+  h1
+    font-size 1.5rem
+  .tags
+    display flex
+    flex-wrap nowrap
+    justify-content center
+    p
+      margin 5px
+      border 1px solid #555
+      height 25px
+      line-height 25px
+      font-size .8rem
+      padding 0 5px
+      border-radius 5px
+      cursor pointer
+@media (max-width: 768px) 
+  .swiper-item
+    height 200px
+    padding-top 20px
+    .thumbnail
+      width 50%
+      margin-left -25%
+</style>
+
+
