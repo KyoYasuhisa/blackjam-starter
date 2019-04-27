175be7e6b706599d8b986bfcd6f69d4aeee7b287
Satoru Matsuzaka <44631219+satorumatsuzaka@users.noreply.github.com>
2019-04-27 20:44:57 +0900

    fd

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -304,7 +304,7 @@ export default {
           max-height 60px
           min-width 60px
           min-height 60px
-          border-radius 70%
+          border-radius 50%
           border 2px solid #eee
           margin 0 10px
         p
@@ -324,7 +324,7 @@ export default {
           border-radius 5px
           font-size .8rem
           line-height 1.2rem
-          min-width 50%
+          min-width 70%
       .bubble-i
         display flex
         flex-wrap no-wrap
